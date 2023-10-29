01aad45bb6c872bc921da88df240b466ad9ab516
Nuno Carriço <nuno.carrico.98@gmail.com>
2023-10-29 16:22:10 +0000

    menu done

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject creditsPanel;
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Credits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void BackCredits()
+    {
+        creditsPanel.SetActive(false);
     }
 
     public void Exit()
